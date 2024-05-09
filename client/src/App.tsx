af51a631c9b76ca3da0c3031dfc48f82397d2fe4
mikegag <Michael.gagliardi1@hotmail.com>
2024-05-09 11:07:18 -0400

    server folder test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="bg-red-100 shadow-md rounded-md px-4 py-3">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
