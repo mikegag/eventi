a8016a072353f51f5d8d87f412a410109f2f31ea
mikegag <Michael.gagliardi1@hotmail.com>
2024-06-01 13:05:06 -0400

    minor syntax updates

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -13,36 +13,36 @@ export default function About(){
             <div className="about-top-line" role="presentation"></div>
             <div className="flex flex-col lg:flex-row">
                 <div>
-                <motion.div
-                    ref={ref}
-                    initial={{ opacity: 0, x: "-20%" }}
-                    animate={{ opacity:inView ? 1 : 0, x:inView ? 0 : "-20%" , transition: { duration: 1.1 } }}
-                >
-                    <h3 className="page-title mt-0 no-underline px-4 lg:text-left lg:px-0">
-                        {data.aboutSection.description.heading}
-                    </h3>
-                </motion.div>
-                <motion.div
-                    ref={ref}
-                    initial={{ opacity: 0, x: "20%" }}
-                    animate={{ opacity:inView ? 1 : 0, x:inView ? 0 : "20%" , transition: { duration: 1.1 } }}
-                >
-                    <ul className="mx-auto lg:mr-28 lg:pt-4">
-                        {data.aboutSection.description.points.map((curr, index)=>(
-                            <li 
-                                key={index} 
-                                className="about-bullet-point">
-                                    {curr}
-                            </li>
-                        ))}
-                    </ul>
-                </motion.div>
+                    <motion.div
+                        ref={ref}
+                        initial={{ opacity: 0, x: "-20%" }}
+                        animate={{ opacity:inView ? 1 : 0, x:inView ? 0 : "-20%" , transition: { duration: 1.1 } }}
+                    >
+                        <h3 className="page-title mt-0 no-underline px-4 lg:text-left lg:px-0">
+                            {data.aboutSection.description.heading}
+                        </h3>
+                    </motion.div>
+                    <motion.div
+                        ref={ref}
+                        initial={{ opacity: 0, x: "20%" }}
+                        animate={{ opacity:inView ? 1 : 0, x:inView ? 0 : "20%" , transition: { duration: 1.1 } }}
+                    >
+                        <ul className="mx-auto lg:mr-28 lg:pt-4">
+                            {data.aboutSection.description.points.map((curr, index)=>(
+                                <li 
+                                    key={index} 
+                                    className="about-bullet-point">
+                                        {curr}
+                                </li>
+                            ))}
+                        </ul>
+                    </motion.div>
                 </div>
-                    <img 
-                        src={require("../assets/couple-love.png")} 
-                        className="w-60 mt-8 mb-14 mx-auto lg:w-72 lg:ml-8 lg:mt-14"
-                        alt="man and women holding hands looking at each other"
-                    />
+                <img 
+                    src={require("../assets/couple-love.png")} 
+                    className="w-60 mt-8 mb-14 mx-auto lg:w-72 lg:ml-8 lg:mt-14"
+                    alt="man and women holding hands looking at each other"
+                />
             </div>
         </>
     )
