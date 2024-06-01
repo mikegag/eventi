c50a9a8b3740017784b302ef1f715ca272262352
mikegag <Michael.gagliardi1@hotmail.com>
2024-06-01 13:09:47 -0400

    updated css classes for Example component

--- client/src/components/Example.tsx
+++ client/src/components/Example.tsx
@@ -1,7 +1,7 @@
-import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React from "react"
 import data from "../Data.json"
+import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Link } from "react-router-dom"
 import { motion } from "framer-motion"
 import { useInView } from "react-intersection-observer"
@@ -9,11 +9,10 @@ import { useInView } from "react-intersection-observer"
 export default function Example(){
     const { ref, inView } = useInView({
         threshold: 0.6,
-        triggerOnce:false
     })
 
     return (
-        <div className="flex flex-col lg:flex-row bg-main-color-lightgrey rounded-2xl border-2 border-accent-color-black px-3 py-4 lg:px-5 lg:py-8 mx-auto lg:mx-20">
+        <div className="example-container">
             <img 
                 src={require("../assets/graph-example.png")} 
                 className=" w-72 lg:w-80 rounded-2xl mx-auto lg:mx-5 lg:my-auto"
@@ -25,13 +24,18 @@ export default function Example(){
                     animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : "-10%" , transition: { duration: 1.1 } }}
             >
                 <div className="flex flex-col items-center lg:items-start lg:pl-20 lg:pr-4">
-                    <h3 className="page-title no-underline text-accent-color-black mt-12 lg:mt-0 lg:px-0 mb-7">{data.graphExample.heading}</h3>
-                    <p className=" text-xl text-center lg:text-left lg:text-2xl font-medium mb-10 lg:mb-10">{data.graphExample.body}</p>
-                    <Link to={'/d'}>
+                    <h3 className="page-title no-underline text-accent-color-black mt-12 lg:mt-0 lg:px-0 mb-7">
+                        {data.graphExample.heading}
+                    </h3>
+                    <p className=" text-xl text-center lg:text-left lg:text-2xl font-medium mb-10 lg:mb-10">
+                        {data.graphExample.body}
+                    </p>
+                    <Link to={'/signup'}>
                         <motion.button
                             whileHover={{ scale: 1.06 }}
                             whileTap={{ scale: 0.9 }}
                             className="header-btn bg-main-color-yellow text-xl px-7 py-2 hover:bg-accent-color-darkyellow shadow-sm"
+                            aria-label="redirects user to sign up page"
                         >
                             Explore
                             <FontAwesomeIcon icon={faArrowRightLong} className="ml-5 my-auto"/> 
