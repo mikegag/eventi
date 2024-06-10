f30d807f6a086e75b686c41780c3bbd6ff47e006
mikegag <Michael.gagliardi1@hotmail.com>
2024-06-10 12:07:28 -0400

    updated and registered models within admin.py

--- server/eventi_app/admin.py
+++ server/eventi_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import DateIdea, User, Profile
 
-# Register your models here.
+admin.site.register(DateIdea)
+admin.site.register(User)
+admin.site.register(Profile)
